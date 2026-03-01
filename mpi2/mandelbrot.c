98c1c459d2e99023a9f18f9029813358e447224b
liamscarlett <liam.scarlett@outlook.com>
2026-03-01 20:39:20 +0800

    add comments to serial mandelbrot codes

--- mpi2/mandelbrot.c
+++ mpi2/mandelbrot.c
@@ -17,19 +17,37 @@ int main() {
 
     float complex   z, kappa;
 
-    x=(float **) malloc(N*sizeof(float *));
-    for (i=0;i<N;i++)
-        x[i]=(float *) malloc(N*sizeof(float));
+    // Allocate x as a 2-D array - requires use of a double pointer
+    x=(float**)malloc(N*sizeof(float*));
+    for (i=0;i<N;i++) {
+      x[i]=(float*)malloc(N*sizeof(float));
+    }
   
+    // Iterate over 2-D grid: i covers N cols and j covers N rows
     for (i=0; i<N; i++) {
         for (j=0; j<N; j++) {
-	    z=kappa= (4.0*(i-N/2))/N + (4.0*(j-N/2))/N * I;
+	    
+            //kappa is the coordinate in the complex plane of the (i,j)th pixel
+            //both the real and imaginary axis range from -2 to 2 (4 units)
+            kappa = (4.0*(i-N/2))/N + (4.0*(j-N/2))/N * I;
+
+            //z_0 = kappa, initial value of z before iteration begins
+            z = kappa;
 	
-	    k=1;
-	    while ((cabs(z)<=2) && (k++<MAXITER)) 
-	        z= z*z + kappa;
+            //now we iterate until either the max number of iterations is reached 
+            //OR z leaves the circle of radius 2 centred at the origin
+	    k=1; 
+	    while ((cabs(z)<=2) && (k++<MAXITER)) {
+	        z = z*z + kappa;
+            }
 	  
-	    x[i][j]= log((float)k) / log((float)MAXITER);
+            // Store the number of iterations required for this point to leave the mandelbrot set.
+            // If this point IS IN the Mandelbrot set, then k=MAXITER.
+            // We actually store log(k)/log(MAXITER) to see more colour variation in the points 
+            // outside the Mandelbrot set.
+            // Points that are in the set have k=MAXITER and therefore log(k)/log(MAXITER)=1.0.
+            // When the mandelbrot.ppm file is created below, x=1.0 translates to 100% blue.
+	    x[i][j]=log((float)k) / log((float)MAXITER);
         }
 
         tenth=(int) N/10;
--- mpi2/mandelbrot2.c
+++ mpi2/mandelbrot2.c
@@ -18,16 +18,29 @@ int main() {
     float complex   z, kappa;
 
     x=(float *) malloc(N*N*sizeof(float));
-  
+ 
+    //Loop over N^2 pixels
     for (loop=0; loop<N*N; loop++) {
-	    i=loop%N;
-	    j=loop/N;
+            // i:cols and j:rows
+	    
+            // i=mod(loop,N) means i iterates from 0...N-1 then back to 0...N-1 etc.
+            i=loop%N;
 
-	    z=kappa= (4.0*(i-N/2))/N + (4.0*(j-N/2))/N * I;
+            // j=loop/N is integer division.
+            // For the first  round of i=0...N-1, loop < N, and hence j=0.
+            // For the second round of i=0...N-1, N <= loop < 2*N, and hence j=1.
+            // This way we fix the row and iterate over all columns 
+            // and then incrememt the row the iterate over all columns again, etc...
+            j=loop/N;
+
+	    kappa= (4.0*(i-N/2))/N + (4.0*(j-N/2))/N * I;
+
+            z=kappa;
 	
 	    k=1;
-	    while ((cabs(z)<=2) && (k++<MAXITER)) 
+	    while ((cabs(z)<=2) && (k++<MAXITER)) {
 	        z= z*z + kappa;
+            }
 	  
 	    x[loop]= log((float)k) / log((float)MAXITER);
     }
